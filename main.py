6fcf8d2430535ffd97838e45b7bbb8caec1dcf33
Alexander <a.rohrig@me.com>
2020-09-08 16:20:17 -0700

    Added templates
    Added templates
    Modified app
    Modified main

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+userLanguage = 'en'
+
 @app.route('/', methods=['GET'])
 def hello():
-    return "Hello World"
+	print(userLanguage)
+	if userLanguage == 'it':
+		return render_template('index2_IT.html')
+	elif userLanguage == 'de':
+		return render_template('index2_DE.html')
+	elif userLanguage == 'zh':
+		return render_template('index2_zhHans.html')
+	else:
+		return render_template('index2.html')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
